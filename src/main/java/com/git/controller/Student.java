7b4925d691a20486afa9db86e049e9f60a01ab43
hushuang <542518880@qq.com>
2024-04-24 15:58:03 +0800

    第一次项目提交

---
+++ src/main/java/com/git/controller/Student.java
@@ -0,0 +1,12 @@
+package com.git.controller;
+
+public class Student {
+    private String name;//姓名
+    private int age;//年龄
+    private String sex;//性别
+    private String address;//地址
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
