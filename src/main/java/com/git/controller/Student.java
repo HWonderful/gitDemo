4b74b5bbedaa10c0e54209cd48bd838d366762c6
hushuang <542518880@qq.com>
2024-04-24 16:41:24 +0800

    修改推送

--- src/main/java/com/git/controller/Student.java
+++ src/main/java/com/git/controller/Student.java
@@ -9,5 +9,8 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello git");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
